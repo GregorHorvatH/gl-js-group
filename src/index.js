4e22d0b05c77728395b5c9b469591d0498c4a880
Geri <gregor.horvath83@gmail.com>
2021-07-21 12:00:53 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+console.log('start');
+
+fetch('https://hn.algolia.com/api/v1/search?query=cats')
+  .then((resp) => resp.json())
+  .then((data) => console.log('data:', data));
+
+console.log('end');
